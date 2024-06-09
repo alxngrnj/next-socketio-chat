f5265466aaef27216400fe3d853c9af951ef476f
Alexon Granja <alexongranja@gmail.com>
2024-06-09 00:53:50 -0300

    feat(middleware): Error handling middleware implemented

---
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import Logger from '../core/Logger';
+import { ApiError, GenericError, ErrorType } from '../core/ApiError';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _next: NextFunction,
+) => {
+  if (err instanceof ApiError) {
+    ApiError.handle(err, res);
+    if (err.type === ErrorType.INTERNAL)
+      Logger.error(
+        `500 - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`,
+      );
+  } else {
+    Logger.error(
+      `500 - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`,
+    );
+    Logger.error(err);
+    ApiError.handle(new GenericError(), res);
+  }
+};
